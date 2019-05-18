8eed9ccb33672ceb071205e054e56757b9d9199d
Anthony Arias <aarias011@west-mec.org>
2019-05-17 18:18:24 -0700

    JavaScript Added
    Form validation for the contact form added using JavaScript. All website content and styles completed. Comments must be added.

--- LAP.js
+++ LAP.js
@@ -14,3 +14,54 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+//Form Validation
+window.addEventListener("load", function () {
+    document.getElementById("submitButton").onclick = runSubmit;
+    document.getElementById("emailInput").oninput = validateEmail;
+    document.getElementById("nameInput").oninput = validateName;
+    document.getElementById("subjectInput").oninput = validateSubject;
+    document.getElementById("textBox").oninput = validateTextBox;
+});
+
+function runSubmit() {
+    validateName();
+    validateEmail();
+    validateSubject();
+    validateText();
+}
+
+function validateEmail() {
+    var email = document.getElementById("emailInput");
+    if (email.validity.valueMissing) {
+        email.setCustomValidity("Please enter a valid email");
+    } else {
+        email.setCustomValidity("");
+    }
+}
+
+function validateName() {
+    var name = document.getElementById("nameInput");
+    if (name.validity.valueMissing) {
+        name.setCustomValidity("Please enter your full name");
+    } else {
+        name.setCustomValidity("");
+    }
+}
+
+function validateSubject() {
+    var subject = document.getElementById("subjectInput");
+    if (subject.validity.valueMissing) {
+        subject.setCustomValidity("Please enter a subject here");
+    } else {
+        subject.setCustomValidity("");
+    }
+}
+
+function validateTextBox() {
+    var textBox = document.getElementById("textBox");
+    if (textBox.validity.valueMissing) {
+        textBox.setCustomValidity("Please write your message here");
+    } else {
+        textBox.setCustomValidity("");
+    }
+}
