549641c673567124ae1c7942746e05f25bfdfc3e
Unknown <mrizo036@west-mec.org>
2019-05-19 09:54:28 -0700

    Final Commit
    All HTML, CSS, and JavaScript files have been completed. The pages are functional when displayed on both mobile and larger devices. Comments have been added. Ready for Review.

--- LAPS_javascript.js
+++ LAPS_javascript.js
@@ -1,4 +1,4 @@
-// The following function causes the slide show displayed in the Index page to move automatically
+// The following function causes the slide show displayed in the Index page to move automatically, changing the images every two seconds.
 var myIndex = 0;
 carousel();
 
@@ -15,7 +15,9 @@ function carousel() {
   x[myIndex - 1].style.display = "block";
   setTimeout(carousel, 2000); // Change image every 2 seconds
 }
+
 //Form Validation
+// The code block below runs each of the following functions upon the user either clicking the element or typing information into the element.
 window.addEventListener("load", function () {
   document.getElementById("submitButton").onclick = runSubmit;
   document.getElementById("emailInput").oninput = validateEmail;
@@ -24,6 +26,7 @@ window.addEventListener("load", function () {
   document.getElementById("textBox").oninput = validateTextBox;
 });
 
+// The following code block creates a function which will run all of the other validation functions that are created within this document. This will allow for the page to stop the user from submitting their information if they have forgotten a certain section of the form.
 function runSubmit() {
   validateName();
   validateEmail();
@@ -31,6 +34,7 @@ function runSubmit() {
   validateText();
 }
 
+// The following functions all serve they same purpose. They allow for the error message that is displayed when the user forgets to input certain information to be set to something else rather than the default text. this allows for the massage to specify what information the user should put into any field that they missed.
 function validateEmail() {
   var email = document.getElementById("emailInput");
   if (email.validity.valueMissing) {
@@ -66,3 +70,10 @@ function validateTextBox() {
     textBox.setCustomValidity("");
   }
 }
+
+// Email Array
+// The array below serves to store the emails which are inputted by the user when enterign one into the contact form. Due to the time contraints of the project, we were not able to finish this section before the due date and thus it does not work properly.
+var userEmails = [];
+var email = document.getElementById("emailInput").innerHTML;
+
+userEmails.push(email);
